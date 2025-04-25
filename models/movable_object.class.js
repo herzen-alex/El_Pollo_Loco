1e8dc3bed71c9f17243b3e56defcc533360cbaec
herzen-alex <herzen.alex1@web.de>
2025-04-25 08:04:55 +0200

    added

--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -1,112 +1,76 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    width = 100;
-    height = 150;
-    img;
-    imageCache = {};
-    currentImage = 0;
-    speed = 0.15;
-    otherDirection = false;
-    speedY = 0;
-    acceleration = 2.5;
-    lastHit = 0;
-    energy = 100;
+class MovableObject extends DrawableObject {
+  speed = 0.15;
+  otherDirection = false;
+  speedY = 0;
+  acceleration = 2.5;
+  lastHit = 0;
+  energy = 100;
 
-    offset = {
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
+  offset = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
+
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 150;
+  }
+
+  // character.isColliding(chicken);
+  isColliding(mo) {
+    return (
+      this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
+      this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
+      this.x + this.offset.left < mo.x - mo.offset.right &&
+      this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom
+    );
+  }
+
+  hit() {
+    this.energy -= 5;
+    if (this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
     }
+  }
 
-    applyGravity() {
-        setInterval(() => {
-            if (this.isAboveGround() || this.speedY > 0) {
-            this.y -= this.speedY;
-            this.speedY -= this.acceleration;
-            }
-        }, 1000 / 25);
-    }
+  isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit; // Difference in ms
+    timepassed = timepassed / 1000; // Difference in s
+    return timepassed < 0.5;
+  }
 
-    isAboveGround() {
-        return this.y < 150;
-    }
+  isDead() {
+    return this.energy == 0;
+  }
 
-    loadImage(path) {
-        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
-        this.img.src = path;
-    }
+  moveRight() {
+    this.x += this.speed;
+  }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+  moveLeft() {
+    this.x -= this.speed;
+  }
 
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken || this instanceof Small_Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();  
-        }
-    }
+  jump() {
+    this.speedY = 30;
+  }
 
-    // character.isColliding(chicken);
-    isColliding(mo) {
-        return this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
-            this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
-            this.x + this.offset.left < mo.x - mo.offset.right &&
-            this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom
-        }
-
-        hit() {
-            this.energy -= 5;
-            if (this.energy < 0) {
-                this.energy = 0;
-            } else {
-                this.lastHit = new Date().getTime();
-            }
-        }
-
-        isHurt() {
-            let timepassed = new Date().getTime() - this.lastHit; // Difference in ms 
-            timepassed = timepassed / 1000; // Difference in s
-            return timepassed < 0.5;
-        }
-
-        isDead() {
-            return this.energy == 0;
-        }
-
-    /**
-     * 
-     * @param {Array} arr - [img/image1.png, img/image2.png, ....]
-     */
-    loadImages(arr) {
-        arr.forEach( (path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
-    moveRight() {
-        this.x += this.speed; 
-    }
-
-    moveLeft() {
-        this.x -= this.speed;
-    }
-
-    jump() {
-        this.speedY = 30;
-    }
-    
-    playAnimation(images) {
-        let i = this.currentImage % images.length; // let i = 0 (%- Rest) 6 .....=> let i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5........
-            let path = images[i];
-            this.img = this.imageCache[path];
-            this.currentImage ++;
-    }
+  playAnimation(images) {
+    let i = this.currentImage % images.length; // let i = 0 (%- Rest) 6 .....=> let i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5........
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,12 +1,12 @@
 class World {
+
     character = new Character();
-
     level = level1;
-
     canvas;
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -39,6 +39,7 @@ class World {
 
         this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
+        this.addToMap(this.statusBar);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.coins);
